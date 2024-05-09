d156983056238ba65ab723dd883483e0af424037
BERMOND Numa <numa.bermond@telecomnancy.eu>
2024-05-09 20:55:18 +0000

    Update selectRandomMap.h

--- selectRandomMap.h
+++ selectRandomMap.h
@@ -1,3 +1,7 @@
 int randomMapIndex();
 char *getMapFromIndex(int index);
 char *getColliderTable(int index);
+char *getMapHoles(int index);
+int randomInt(int max);
+void getCoordFromTiles(int tileNumber, int *xTile, int *yTile);
+int *convertirFichierEnTableau(const char *nomFichier, int *taille);
