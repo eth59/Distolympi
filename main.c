c0f3c0b459bc157e2ecbd3222aff128f5e573a81
Ethan.Quilliot <ethan.quilliot@telecomnancy.eu>
2024-05-13 14:47:31 +0200

    Bienvenue sur le site de la formation Git

--- main.c
+++ main.c
@@ -37,7 +37,7 @@ int main() {
         exit(-1);
     }
 
-    SDL_SetWindowFullscreen(window,SDL_WINDOW_FULLSCREEN);
+    // SDL_SetWindowFullscreen(window,SDL_WINDOW_FULLSCREEN);
 
     // Initialisation du renderer
     SDL_Renderer *renderer;
