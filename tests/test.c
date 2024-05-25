469c548b919c9b57ce22dc0ab1af3c37b59a48ed
BERMOND Numa <numa.bermond@telecomnancy.eu>
2024-05-25 14:11:06 +0000

    Update test.c

--- tests/test.c
+++ tests/test.c
@@ -20,6 +20,15 @@ int main(void) {
     // RUN_TEST(test_check_object_collision);
     // RUN_TEST(test_generate_random_objects);
 
+    // tests pour collisionHandling
+    RUN_TEST(test_openCollisionFile);
+    RUN_TEST(test_checkCollision);
+
+    // tests pour la séléction aléatoire de la map
+    RUN_TEST(test_randomMapIndex);
+    RUN_TEST(test_getMapFromIndex);
+    RUN_TEST(test_getColliderTable);
+
     UNITY_END();
     return 0;
 }
