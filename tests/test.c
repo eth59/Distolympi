a35475cd900584fb4eae390b74e250a0d19223f0
CHATONNIER Nino <nino.chatonnier@telecomnancy.eu>
2024-05-25 17:31:51 +0200

    tests nino

--- tests/test.c
+++ tests/test.c
@@ -1,24 +1,37 @@
+#include "../Unity/src/unity.h"
 #include "testInventory.h"
 #include "testObjects.h"
-#include "../Unity/src/unity.h"
+#include "testAnimations.h"
+#include "testCharacter.h"
 
 void setUp()
 {
-
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_PNG);
 }
 
 void tearDown()
-{
-
+{ 
+    IMG_Quit();
+    SDL_Quit();
 }
 
 int main(void) {
     UNITY_BEGIN();
 
-    RUN_TEST(test_init_objects);
-    // RUN_TEST(test_use_selected_objects_and_use_object);
-    // RUN_TEST(test_check_object_collision);
-    // RUN_TEST(test_generate_random_objects);
+    /*RUN_TEST(test_init_objects);
+    //RUN_TEST(test_use_selected_objects_and_use_object);
+    RUN_TEST(test_check_object_collision);
+    RUN_TEST(test_generate_random_objects);
+
+    */
+    RUN_TEST(test_get_texture_valid_image);
+    RUN_TEST(test_get_frames);
+
+    //
+    RUN_TEST(test_get_direction_and_move);
+    RUN_TEST(test_get_Rectdest_attacks);
+    RUN_TEST(test_get_melee_direction);
 
     UNITY_END();
     return 0;
---
+++ tests/testCharacter.h
@@ -0,0 +1,6 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+void test_get_direction_and_move(void);
+void test_get_Rectdest_attacks(void);
+void test_get_melee_direction(void);
